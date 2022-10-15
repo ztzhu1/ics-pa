917827fcef35ed20a1df96bd71e1b5507bf16677
ztzhu1 <zhuzitian@zju.edu.cn>
2022-10-15 20:13:00 +0800

    Try to complete pseudo instruction

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -17,26 +17,52 @@ static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  rtl_xori(s, dest, src1, -1);
 }
 
 static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- -src1
-  TODO();
+  rtl_sub(s, dest, rz, src1);
 }
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  rtl_addi(s, dest, rz, -1);
+  rtl_slli(s, dest, dest, width * 8 - 1);
+  // cheating temporarily
+  if (*src1 >> (width * 8 - 1)) {
+    rtl_or(s, dest, src1, dest);
+  }
+  else {
+    rtl_xori(s, dest, dest, -1);
+    rtl_and(s, dest, src1, dest);
+  }
+}
+
+static inline def_rtl(sext_bit, rtlreg_t* dest, const rtlreg_t* src1, int bit) {
+  // dest <- signext(src1[(bit - 1) .. 0])
+  rtl_addi(s, dest, rz, -1);
+  rtl_slli(s, dest, dest, bit - 1);
+  // cheating temporarily
+  if (*src1 >> (bit - 1)) {
+    rtl_or(s, dest, src1, dest);
+  }
+  else {
+    rtl_xori(s, dest, dest, -1);
+    rtl_and(s, dest, src1, dest);
+  }
 }
 
 static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- zeroext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  rtl_addi(s, dest, rz, -1);
+  rtl_slli(s, dest, dest, width * 8 - 1);
+  rtl_xori(s, dest, dest, -1);
+  rtl_and(s, dest, src1, dest);
 }
 
 static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  rtl_srli(s, dest, src1, width * 8 - 1);
 }
 #endif
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,3 +1,30 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
+
+def_EHelper(auipc) {
+  printf("auipc: 0x%08x\n", s->pc);
+  rtl_addi(s, ddest, &s->pc, id_src1->imm);
+}
+
+def_EHelper(jal) {
+  // cheating 
+  printf("jal: 0x%08x\n", cpu.pc);
+  printf("%08x\n", id_src1->imm);
+  if (id_src1->imm >> 19)
+    *ddest = id_src1->imm | 0xFFF00000;
+  else
+    *ddest = id_src1->imm & 0x000FFFFF;
+  printf("%08x\n", *ddest);
+  for (int j = 0; j < 10; j++)
+    printf("%08x\n", vaddr_ifetch(0x80000000+4*j, 4));
+  // rtl_add(s, &s->dnpc, &s->pc, ddest);
+  // rtl_addi(s, ddest, &s->pc, 4);
+}
+
+def_EHelper(addi) {
+  printf("addi: 0x%08x\n", s->pc);
+  rtl_sext_bit(s, ddest, &id_src2->imm, 12);
+  printf("%d, %d\n", id_src2->imm, *ddest);
+  rtl_addi(s, ddest, dsrc1, *ddest);
+}
