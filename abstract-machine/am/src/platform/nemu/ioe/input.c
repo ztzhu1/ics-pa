2f47207c95955dcb798f3ad5d4dda2c4272e5c0d
ztzhu1 <zhuzitian@zju.edu.cn>
2022-10-20 00:38:14 +0800

    Finish AM keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,21 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 
+static bool initKbd = false;
+
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  if (!initKbd) {
+    kbd->keydown = 0;
+    kbd->keycode = AM_KEY_NONE;
+    initKbd = true;
+  }
+
+  uint32_t k = *(uint32_t *)KBD_ADDR;
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
+  if (kbd->keycode != 0)
+    printf("%d\n", kbd->keycode);
 }
