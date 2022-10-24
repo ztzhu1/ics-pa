e1977b3e85fc9b77d144b02c30abf6964b5ae786
ztzhu1 <zhuzitian@zju.edu.cn>
2022-10-24 13:15:25 +0800

    Finish difftest

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -1,9 +1,21 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
+#include <common.h>
+
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool ok = true;
+  for (int i = 0; i < 32; i++) {
+      bool ok_temp = difftest_check_reg(reg_name(i, 4), pc, ref_r->gpr[i]._32, cpu.gpr[i]._32);
+      if (ok == true)
+        ok = ok_temp;
+  }
+  if (ref_r->pc != pc) {
+    ok = false;
+    Log("PC is different, right = %08x, wrong = %08x", ref_r->pc, pc);
+  }
+  return ok;
 }
 
 void isa_difftest_attach() {
