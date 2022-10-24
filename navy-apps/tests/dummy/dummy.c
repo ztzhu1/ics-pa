d5e4172dd8cb081ff6d7f8942ae8b4c32510b2c5
ztzhu1 <zhuzitian@zju.edu.cn>
2022-10-24 23:05:15 +0800

    Original file

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,16 +1,12 @@
-// #include <stdint.h>
-#include <stdio.h>
-// 
-// 
-// #ifdef __ISA_NATIVE__
-// #error can not support ISA=native
-// #endif
+#include <stdint.h>
 
-// #define SYS_yield 1
-// extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
+#ifdef __ISA_NATIVE__
+#error can not support ISA=native
+#endif
+
+#define SYS_yield 1
+extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-//  return _syscall_(SYS_yield, 0, 0, 0);
-  printf("@@@@@@@@@@@@\n");
-  return 0;
+  return _syscall_(SYS_yield, 0, 0, 0);
 }
