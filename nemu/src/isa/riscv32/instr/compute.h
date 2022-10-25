3e7bddcb922949d7c6eafa89823142a4f0553fc8
ztzhu1 <zhuzitian@zju.edu.cn>
2022-10-25 08:42:58 +0800

    Put the line modifying destination to the last

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -208,8 +208,8 @@ def_EHelper(sltiu) {
 }
 
 def_EHelper(jal) {
-  rtl_addi(s, ddest, &s->pc, 4);
   rtl_addi(s, &s->dnpc, &s->pc, id_src1->imm);
+  rtl_addi(s, ddest, &s->pc, 4);
 }
 
 def_EHelper(jalr) {
