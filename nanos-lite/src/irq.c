025ffba263dc0698608da232dbce823f7ff446ac
ztzhu1 <zhuzitian@zju.edu.cn>
2022-10-25 08:34:19 +0800

    Delete redundant cases

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,13 +4,8 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_NULL: printf("event null\n"); break;
     case EVENT_YIELD: printf("event yield\n"); break;
     case EVENT_SYSCALL: do_syscall(c); break;
-    case EVENT_PAGEFAULT: printf("event pagefault\n"); break;
-    case EVENT_ERROR: printf("event error\n"); break;
-    case EVENT_IRQ_TIMER: printf("event timer\n"); break;
-    case EVENT_IRQ_IODEV: printf("event iodev\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   return c;
