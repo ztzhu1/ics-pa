bb00aa5de3c61163ca0918990a2c734c93bd818c
ztzhu1 <zhuzitian@zju.edu.cn>
2022-10-27 14:44:13 +0800

    Partly finish pa3
    For time reason, I ignored some apps, including flappy bird and pal. It's a shame.

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -15,11 +15,7 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-int SDL_WaitEvent(SDL_Event *event) {
+int SDL_PollEvent(SDL_Event *event) {
   int count = NDL_PollEvent(buf, sizeof(buf));
   if (count == 0)
     return 0;
@@ -42,7 +38,31 @@ int SDL_WaitEvent(SDL_Event *event) {
       break;
     }
   }
-  return 0;
+  return 1;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  while(NDL_PollEvent(buf, sizeof(buf)) == 0);
+  char key[20];
+  char *ptr = buf;
+  while (*ptr != 'k') ptr++;
+  switch (*++ptr) {
+    case 'd' : event->type = SDL_KEYDOWN; break;
+    case 'u' : event->type = SDL_KEYUP; break;
+  }
+  while (*ptr != ' ') ptr++;
+  ptr++;
+  int i;
+  for (i = 0; *ptr != '\n'; i++)
+    key[i] = *ptr++;
+  key[i] = '\0';
+  for (i = 0; i < SDLK_KEY_NUM; i++) {
+    if (strcmp(key, keyname[i]) == 0) {
+      event->key.keysym.sym = i;
+      break;
+    }
+  }
+  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
